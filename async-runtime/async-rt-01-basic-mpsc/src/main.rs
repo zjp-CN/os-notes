563d8bd1265eab881e4123ee2f55b99b7cf324ac
zjp <jiping_zhou@foxmail.com>
2024-12-12 09:32:03 +0800

    async-rt-01-basic-mpsc: recite

--- async-runtime/async-rt-01-basic-mpsc/src/main.rs
+++ async-runtime/async-rt-01-basic-mpsc/src/main.rs
@@ -1,124 +1,120 @@
 use std::{
-    future::Future,
+    pin::Pin,
     sync::{
         Arc, Mutex,
-        mpsc::{Receiver, SyncSender, sync_channel},
+        mpsc::{Receiver, Sender, channel},
     },
-    task::{Context, Poll, Waker},
+    task::{Context, Poll, Wake, Waker},
     thread,
     time::Duration,
 };
 
-use futures::{
-    future::{BoxFuture, FutureExt},
-    task::{ArcWake, waker_ref},
-};
-
-struct SharedState {
-    completed: bool,
-    waker: Option<Waker>,
+fn main() {
+    let (sender, receiver) = channel();
+    Executor { sender, receiver }.run(TimerFuture::new(2.0));
 }
 
-pub struct TimerFuture {
-    shared_state: Arc<Mutex<SharedState>>,
+struct MyWaker {
+    task: Mutex<Task>,
+    sender: Sender<Arc<Self>>,
 }
 
-impl Future for TimerFuture {
-    type Output = ();
-    fn poll(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Self::Output> {
-        let mut shared_state = self.shared_state.lock().unwrap();
-        if shared_state.completed {
-            return Poll::Ready(());
-        }
-        shared_state.waker = Some(cx.waker().clone());
-        return Poll::Pending;
+impl Wake for MyWaker {
+    fn wake(self: Arc<Self>) {
+        self.wake_by_ref();
     }
-}
-
-impl TimerFuture {
-    fn new(duration: Duration) -> Self {
-        let shared_state = Arc::new(Mutex::new(SharedState {
-            completed: false,
-            waker: None,
-        }));
-        let thread_shared_state = Arc::clone(&shared_state);
-        thread::spawn(move || {
-            thread::sleep(duration);
-            let mut shared_state = thread_shared_state.lock().unwrap();
-            shared_state.completed = true;
-            if let Some(waker) = shared_state.waker.take() {
-                waker.wake();
-            }
-        });
-        TimerFuture { shared_state }
-    }
-}
-
-struct Executor {
-    task_queue: Receiver<Arc<Task>>,
-}
-
-impl Executor {
-    fn run(&self) {
-        while let Ok(task) = self.task_queue.recv() {
-            let mut fut = task.future.lock().unwrap();
-            let waker = waker_ref(&task);
-            let context = &mut Context::from_waker(&waker);
-            if fut.as_mut().poll(context).is_ready() {
-                println!("The future is done running");
-            }
-        }
-    }
-
-    fn executor_and_spawner() -> (Executor, Spawner) {
-        let (sync_sender, receiver) = sync_channel(10000);
-        let executor = Executor {
-            task_queue: receiver,
-        };
-        let spawner = Spawner {
-            task_sender: sync_sender,
-        };
-        (executor, spawner)
+    fn wake_by_ref(self: &Arc<Self>) {
+        self.sender.send(self.clone()).unwrap();
     }
 }
 
 struct Task {
-    future: Mutex<BoxFuture<'static, ()>>,
-    task_sender: SyncSender<Arc<Task>>,
+    fut: Pin<Box<dyn Send + Future<Output = ()>>>,
 }
 
-impl ArcWake for Task {
-    fn wake_by_ref(arc_self: &Arc<Self>) {
-        let arc_clone = Arc::clone(&arc_self);
-        arc_self.task_sender.send(arc_clone).unwrap();
+impl Future for Task {
+    type Output = ();
+
+    fn poll(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        self.fut.as_mut().poll(cx)
     }
 }
 
-struct Spawner {
-    task_sender: SyncSender<Arc<Task>>,
+struct SharedState {
+    complete: bool,
+    waker: Option<Waker>,
 }
 
-impl Spawner {
-    fn spawn(&self, future: impl Future<Output = ()> + Send + 'static) {
-        let box_future = future.boxed();
-        let task = Arc::new(Task {
-            future: Mutex::new(box_future),
-            task_sender: self.task_sender.clone(),
+struct TimerFuture {
+    state: Arc<Mutex<SharedState>>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+
+    fn poll(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut shared_state = self.state.lock().unwrap();
+        if shared_state.complete {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
+
+impl TimerFuture {
+    fn new(secs: f32) -> Self {
+        let state = Arc::new(Mutex::new(SharedState {
+            complete: false,
+            waker: None,
+        }));
+        thread::spawn({
+            let state = state.clone();
+            move || {
+                thread::sleep(Duration::from_secs_f32(secs));
+                let mut lock = state.lock().unwrap();
+                lock.complete = true;
+                lock.waker.as_mut().unwrap().wake_by_ref();
+                println!("Completed");
+            }
         });
-        self.task_sender.send(task).unwrap();
+        TimerFuture { state }
     }
 }
 
-fn main() {
-    let (executor, spawner) = Executor::executor_and_spawner();
-    spawner.spawn(async {
-        println!("Hello");
-        TimerFuture::new(Duration::from_secs(2)).await;
-        println!("World");
-    });
-    drop(spawner);
-    executor.run();
+struct Executor {
+    receiver: Receiver<Arc<MyWaker>>,
+    sender: Sender<Arc<MyWaker>>,
+}
+
+impl Executor {
+    fn run(&self, fut: impl 'static + Send + Future<Output = ()>) {
+        let my_waker = {
+            let task = Task { fut: Box::pin(fut) };
+            let my_waker = Arc::new(MyWaker {
+                task: Mutex::new(task),
+                sender: self.sender.clone(),
+            });
+            my_waker.sender.send(my_waker.clone()).unwrap();
+            my_waker
+        };
+        let waker = Waker::from(my_waker);
+        let cx = &mut Context::from_waker(&waker);
+        while let Ok(w) = self.receiver.recv() {
+            match w.task.lock().unwrap().fut.as_mut().poll(cx) {
+                Poll::Ready(_) => {
+                    println!("Done");
+                    return;
+                }
+                Poll::Pending => eprintln!("Pending"),
+            };
+        }
+    }
 }
