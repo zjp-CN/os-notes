411581bf795561431de255feef1e4b0b6d27b33b
zjp <jiping_zhou@foxmail.com>
2024-12-12 09:29:29 +0800

    async-rt-01-basic-mpsc
    ref: https://redixhumayun.github.io/async/2024/08/05/async-runtimes.html

---
+++ async-runtime/async-rt-01-basic-mpsc/src/main.rs
@@ -0,0 +1,124 @@
+use std::{
+    future::Future,
+    sync::{
+        Arc, Mutex,
+        mpsc::{Receiver, SyncSender, sync_channel},
+    },
+    task::{Context, Poll, Waker},
+    thread,
+    time::Duration,
+};
+
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{ArcWake, waker_ref},
+};
+
+struct SharedState {
+    completed: bool,
+    waker: Option<Waker>,
+}
+
+pub struct TimerFuture {
+    shared_state: Arc<Mutex<SharedState>>,
+}
+
+impl Future for TimerFuture {
+    type Output = ();
+    fn poll(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+        if shared_state.completed {
+            return Poll::Ready(());
+        }
+        shared_state.waker = Some(cx.waker().clone());
+        return Poll::Pending;
+    }
+}
+
+impl TimerFuture {
+    fn new(duration: Duration) -> Self {
+        let shared_state = Arc::new(Mutex::new(SharedState {
+            completed: false,
+            waker: None,
+        }));
+        let thread_shared_state = Arc::clone(&shared_state);
+        thread::spawn(move || {
+            thread::sleep(duration);
+            let mut shared_state = thread_shared_state.lock().unwrap();
+            shared_state.completed = true;
+            if let Some(waker) = shared_state.waker.take() {
+                waker.wake();
+            }
+        });
+        TimerFuture { shared_state }
+    }
+}
+
+struct Executor {
+    task_queue: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn run(&self) {
+        while let Ok(task) = self.task_queue.recv() {
+            let mut fut = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let context = &mut Context::from_waker(&waker);
+            if fut.as_mut().poll(context).is_ready() {
+                println!("The future is done running");
+            }
+        }
+    }
+
+    fn executor_and_spawner() -> (Executor, Spawner) {
+        let (sync_sender, receiver) = sync_channel(10000);
+        let executor = Executor {
+            task_queue: receiver,
+        };
+        let spawner = Spawner {
+            task_sender: sync_sender,
+        };
+        (executor, spawner)
+    }
+}
+
+struct Task {
+    future: Mutex<BoxFuture<'static, ()>>,
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let arc_clone = Arc::clone(&arc_self);
+        arc_self.task_sender.send(arc_clone).unwrap();
+    }
+}
+
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + Send + 'static) {
+        let box_future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(box_future),
+            task_sender: self.task_sender.clone(),
+        });
+        self.task_sender.send(task).unwrap();
+    }
+}
+
+fn main() {
+    let (executor, spawner) = Executor::executor_and_spawner();
+    spawner.spawn(async {
+        println!("Hello");
+        TimerFuture::new(Duration::from_secs(2)).await;
+        println!("World");
+    });
+    drop(spawner);
+    executor.run();
+}
