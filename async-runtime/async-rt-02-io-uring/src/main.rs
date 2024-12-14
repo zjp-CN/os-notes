b90952ddc718d1365a403fad806ea404edd2313e
zjp <jiping_zhou@foxmail.com>
2024-12-14 10:46:00 +0800

    rt02: io uring driver

--- async-runtime/async-rt-02-io-uring/src/main.rs
+++ async-runtime/async-rt-02-io-uring/src/main.rs
@@ -1,4 +1,7 @@
-use std::io::prelude::*;
+use io_uring::{IoUring, cqueue::Entry as CQE, squeue::Entry as SQE};
+use slab::Slab;
+use std::{io::prelude::*, task::Waker};
+
 fn main() -> std::io::Result<()> {
     let mut stream = std::net::TcpStream::connect("127.0.0.1:3456")?;
 
@@ -17,3 +20,69 @@ fn main() -> std::io::Result<()> {
 
     Ok(())
 }
+
+struct Driver {
+    uring: IoUring,
+    slab: Slab<LifeCycle>,
+}
+
+impl Driver {
+    fn new() -> Self {
+        Driver {
+            uring: IoUring::new(128).expect("Failed to initialize io uring."),
+            slab: Slab::with_capacity(128),
+        }
+    }
+
+    /// Submit an IO request: user_data is set up the same as slab index.
+    fn submit(&mut self, entry: SQE) -> usize {
+        let index = self.slab.insert(LifeCycle::Submitted);
+        let entry = entry.user_data(index as u64);
+
+        // safety: must ensure entry is valid
+        unsafe {
+            self.uring
+                .submission()
+                .push(&entry)
+                .expect("submission queue is full")
+        };
+        index
+    }
+
+    /// Block the thread until at least one IO complets.
+    fn wait(&self) {
+        self.uring.submit_and_wait(1).unwrap();
+    }
+
+    fn completion(&mut self) {
+        for cqe in self.uring.completion() {
+            let index = cqe.user_data() as usize;
+            let life_cycle = &mut self.slab[index];
+            match life_cycle {
+                LifeCycle::Submitted => (),
+                LifeCycle::Waiting(waker) => waker.wake_by_ref(),
+                LifeCycle::Completed(entry) => todo!(),
+            }
+            *life_cycle = LifeCycle::Completed(cqe);
+        }
+    }
+
+    fn remove(&mut self, index: usize) {
+        self.slab.remove(index);
+    }
+}
+
+enum LifeCycle {
+    Submitted,
+    Waiting(Waker),
+    Completed(CQE),
+}
+
+struct Op {
+    /// position in slab
+    index: usize,
+    /// buffer that is passed to io_uring and taken away if completed
+    buffer: Option<Vec<u8>>,
+}
+
+struct File {}
