e334f04e083ba8ba91a74d267b6f430f2c67a0b3
zjp <jiping_zhou@foxmail.com>
2024-12-17 10:57:47 +0800

    rt02: read_at takes mutable slice; add async read_to_string

--- async-runtime/async-rt-02-io-uring/src/main.rs
+++ async-runtime/async-rt-02-io-uring/src/main.rs
@@ -12,13 +12,23 @@ mod driver;
 mod executor;
 
 fn main() -> Result<()> {
+    const PATH: &str = "Cargo.toml";
     executor::Executor::block_on(|spawner| async move {
         spawner.spawn(TimerFuture::new(1.0));
 
-        let buf = Vec::with_capacity(1024);
-        let buf = File::read("Cargo.toml", 0, buf).await.unwrap();
-        let content = String::from_utf8(buf).unwrap();
-        println!("{content}");
+        spawner.spawn(async {
+            let mut buf = vec![0; 1024];
+            let read_len = File::read_at("Cargo.toml", 0, &mut buf).await.unwrap();
+            let content = std::str::from_utf8(&buf[..read_len]).unwrap();
+            println!("buf 1024:\n{content}");
+        });
+
+        spawner.spawn(async {
+            println!(
+                "async read_to_string:\n{}",
+                read_to_string(PATH).await.unwrap()
+            );
+        });
     });
 
     Ok(())
@@ -27,8 +37,7 @@ fn main() -> Result<()> {
 struct Op {
     /// position in slab
     index: usize,
-    /// buffer that is passed to io_uring and taken away if completed
-    buffer: Option<Vec<u8>>,
+    buffer_len: usize,
 }
 
 struct File {
@@ -38,7 +47,7 @@ struct File {
 
 impl File {
     /// Read from pos 0 with a fixed-capacity buffer.
-    fn read(path: &str, offset: u64, mut buf: Vec<u8>) -> impl Future<Output = Result<Vec<u8>>> {
+    fn read_at(path: &str, offset: u64, buf: &mut [u8]) -> impl Future<Output = Result<usize>> {
         let mut file = {
             let file = OpenOptions::new()
                 .read(true)
@@ -46,7 +55,7 @@ impl File {
                 .open(path)
                 .unwrap();
             let ptr = dbg!(buf.as_mut_ptr());
-            let len = buf.capacity();
+            let len = buf.len();
 
             let fd = types::Fd(dbg!(file.as_raw_fd()));
             let sqe = opcode::Read::new(fd, ptr, len as _).offset(offset).build();
@@ -56,29 +65,54 @@ impl File {
                 file: Some(file),
                 op: Op {
                     index,
-                    buffer: Some(buf),
+                    buffer_len: len,
                 },
             }
         };
 
         poll_fn(move |cx| {
             if let Some(cqe) = driver::poll(file.op.index, cx.waker()) {
+                driver::remove(file.op.index);
                 let res = cqe.result();
-                let len = if res < 0 {
+                let read_len = if res < 0 {
                     return Poll::Ready(Err(io::Error::from_raw_os_error(-res)));
                 } else {
                     res as usize
                 };
 
-                let mut buffer = file.op.buffer.take().unwrap();
-                // safety: io_uring returns the read length of bytes
-                unsafe { buffer.set_len(len) };
+                assert!(
+                    read_len <= file.op.buffer_len,
+                    "Bytes filled exceed the buffer length."
+                );
+                drop(file.file.take().expect("File has been closed."));
 
-                drop(file.file.take().unwrap());
-                Poll::Ready(Ok(buffer))
+                Poll::Ready(Ok(read_len))
             } else {
                 Poll::Pending
             }
         })
     }
+}
+
+/// Read whole content from a file via filling the buffer block by block.
+pub async fn read_to_string(path: &str) -> Result<String> {
+    const BLOCK: usize = 24;
+    let mut buf = vec![0; BLOCK];
+    let mut pos = 0;
+
+    loop {
+        let read_len = File::read_at(path, pos as u64, &mut buf[pos..pos + BLOCK]).await?;
+        // println!("pos = {pos}, read_len = {read_len}");
+        if read_len == 0 {
+            // EOF
+            break;
+        }
+
+        // move the cursor and allocate a block
+        pos += read_len;
+        buf.resize(pos + BLOCK, 0);
+    }
+
+    // println!("[read_to_string] ret");
+    Ok(String::from_utf8(buf).unwrap())
 }
