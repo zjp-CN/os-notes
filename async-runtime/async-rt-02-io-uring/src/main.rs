e046f216094d0f0e2f1a01f2c5c2ed236305329f
zjp <jiping_zhou@foxmail.com>
2024-12-18 17:08:00 +0800

    rt02: bad read_at API

--- async-runtime/async-rt-02-io-uring/src/main.rs
+++ async-runtime/async-rt-02-io-uring/src/main.rs
@@ -1,7 +1,7 @@
 use executor::TimerFuture;
 use io_uring::{opcode, types};
 use std::{
-    fs::{File as StdFile, OpenOptions},
+    fs::File as StdFile,
     future::poll_fn,
     io::{self, Result},
     os::fd::AsRawFd,
@@ -21,7 +21,7 @@ fn main() {
 
         spawner.spawn(async {
             let mut buf = vec![0; 1024];
-            let read_len = File::read_at("Cargo.toml", 0, &mut buf).await.unwrap();
+            let read_len = read_at("Cargo.toml", 0, &mut buf).await.unwrap();
             let content = std::str::from_utf8(&buf[..read_len]).unwrap();
             println!("buf 1024:\n{content}");
         });
@@ -35,63 +35,50 @@ fn main() {
     });
 }
 
-struct Op {
-    /// position in slab
-    index: usize,
-    buffer_len: usize,
-}
+/// Bad practice with borrowed buffer!!! (Though it seems to work.)
+fn read_at(path: &str, offset: u64, buf: &mut [u8]) -> impl Future<Output = Result<usize>> {
+    struct File {
+        file: Option<StdFile>,
+        index: usize,
+        buffer_len: usize,
+    }
 
-struct File {
-    file: Option<StdFile>,
-    op: Op,
-}
+    let mut file = {
+        let file = StdFile::open(path).unwrap();
+        let ptr = buf.as_mut_ptr();
+        let len = buf.len();
 
-impl File {
-    /// Bad practice with borrowed buffer!!! (Though it seems to work.)
-    fn read_at(path: &str, offset: u64, buf: &mut [u8]) -> impl Future<Output = Result<usize>> {
-        let mut file = {
-            let file = OpenOptions::new()
-                .read(true)
-                .write(true)
-                .open(path)
-                .unwrap();
-            let ptr = dbg!(buf.as_mut_ptr());
-            let len = buf.len();
+        let fd = types::Fd(file.as_raw_fd());
+        let sqe = opcode::Read::new(fd, ptr, len as _).offset(offset).build();
+        File {
+            file: Some(file),
+            index: driver::submit(sqe),
+            buffer_len: len,
+        }
+    };
 
-            let fd = types::Fd(dbg!(file.as_raw_fd()));
-            let sqe = opcode::Read::new(fd, ptr, len as _).offset(offset).build();
-            let index = driver::submit(sqe);
-
-            File {
-                file: Some(file),
-                op: Op {
-                    index,
-                    buffer_len: len,
-                },
-            }
+    poll_fn(move |cx| {
+        let Some(cqe) = driver::poll(file.index, cx.waker()) else {
+            return Poll::Pending;
         };
 
-        poll_fn(move |cx| {
-            let Some(cqe) = driver::poll(file.op.index, cx.waker()) else {
-                return Poll::Pending;
-            };
-            driver::remove(file.op.index);
-            let res = cqe.result();
-            let read_len = if res < 0 {
-                return Poll::Ready(Err(io::Error::from_raw_os_error(-res)));
-            } else {
-                res as usize
-            };
+        driver::remove(file.index);
+        drop(file.file.take().expect("File has been closed."));
 
-            assert!(
-                read_len <= file.op.buffer_len,
-                "Bytes filled exceed the buffer length."
-            );
-            drop(file.file.take().expect("File has been closed."));
+        let res = cqe.result();
+        let read_len = if res < 0 {
+            return Poll::Ready(Err(io::Error::from_raw_os_error(-res)));
+        } else {
+            res as usize
+        };
 
-            Poll::Ready(Ok(read_len))
-        })
-    }
+        assert!(
+            read_len <= file.buffer_len,
+            "Bytes filled exceed the buffer length."
+        );
+
+        Poll::Ready(Ok(read_len))
+    })
 }
 
 /// Read whole content from a file via filling the buffer block by block.
@@ -101,7 +88,7 @@ pub async fn read_to_string(path: &str) -> Result<String> {
     let mut pos = 0;
 
     loop {
-        let read_len = File::read_at(path, pos as u64, &mut buf[pos..pos + BLOCK]).await?;
+        let read_len = read_at(path, pos as u64, &mut buf[pos..pos + BLOCK]).await?;
         // println!("pos = {pos}, read_len = {read_len}");
         if read_len == 0 {
             // EOF
